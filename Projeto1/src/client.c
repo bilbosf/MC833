493a18195a0c7e93181a7ee2523d6caf4d85225c
Lucas Soares <l201867@dac.unicamp.br>
2023-04-21 17:39:55 -0300

    Added gitignore, makefile and file structure

---
+++ Projeto1/src/client.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main(){
+    printf("Código Cliente\n");
+    return (0);
+}
---
+++ Projeto1/src/server.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main(){
+    printf("Código Servidor\n");
+    return (0);
+}
